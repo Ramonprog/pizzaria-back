eb8f28d2811951705cdba916b0c13a159700646b
Ramonprog <programador.ramon@gmail.com>
2023-09-09 12:10:04 -0300

    tratando erros com express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { router } from "./routes/router";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 app.use(router);
-app.use(express.json);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({ error: err.message });
+  }
+
+  return res
+    .status(500)
+    .json({ status: "Error", message: "Internal server Error" });
+});
 
 app.listen(3333, () => console.log("server running"));
