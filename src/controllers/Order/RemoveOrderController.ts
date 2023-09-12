dcc25e2f5c1d30a1545d254cf0747e372f0d2b9d
Ramonprog <programador.ramon@gmail.com>
2023-09-12 20:00:10 -0300

    corrigindo removeOrderController

--- src/controllers/Order/RemoveOrderController.ts
+++ src/controllers/Order/RemoveOrderController.ts
@@ -7,7 +7,7 @@ class RemoveOrderController {
       const order_id = req.query.order_id as string;
       const removeOrder = new RemoveOrderService();
 
-      const order = await removeOrder.execute(order_id);
+      await removeOrder.execute(order_id);
 
       return res.json({ message: "Item removed successfully" });
     } catch (error) {
